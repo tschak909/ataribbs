15eb1d944513fe2555dfcb1b49a2a93d02baddfe
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2014-07-27 02:30:06 -0500

    Pushed externs to globals. Added start of terminal code. Bug in log() so temporarily commented out.

---
+++ bbs/src/globals.h
@@ -0,0 +1,8 @@
+/**
+ * globals.h - Global variable externs here.
+ */
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+extern PrinterFlags config_printflags;
+#endif /* GLOBALS_H */
---
+++ bbs/src/terminal.h
@@ -0,0 +1,12 @@
+/**
+ * terminal.h - Functions to deal with RS232 or potentially other endpoints (Dragon Cart?)
+ */
+
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+int terminal_init();
+int terminal_done();
+int term_driver_open();
+
+#endif

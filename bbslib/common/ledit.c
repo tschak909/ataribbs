4ba2d1d5280c41cbf8208e25817f8c048d3c718e
tschak909 <thom.cherryhomes@gmail.com>
2014-09-15 18:13:13 -0500

    ledit_alloc()

--- bbslib/common/ledit.c
+++ bbslib/common/ledit.c
@@ -6,26 +6,47 @@
 #include <string.h>
 #include "ledit.h"
 
-LineEditRecord* line;
-int num_lines=0;
+LineEditRecord* ledit_line;
+int ledit_num_lines=0;
+LineEditLinkage** linkages;
+LineEditLinkage** ledit_oldlinkages;
 
 void* ledit_alloc()
 {
   return calloc(1,sizeof(LineEditRecord));
 }
 
+void * ledit_linkage_alloc(int numLines_to_add)
+{
+  size_t linkagePointersToAlloc;
+  int oldNumLines = ledit_num_lines;
+  int i;
+
+  ledit_num_lines = ledit_num_lines+numLines_to_add;
+
+  ledit_oldlinkages=linkages; // Save it in case realloc fails.
+  linkagePointersToAlloc = sizeof(LineEditLinkage *)*ledit_num_lines;
+  linkages = realloc(linkages,linkagePointersToAlloc);
+
+  for (i=oldNumLines;i<ledit_num_lines;++i)
+    {
+      linkages[i]=calloc(1,sizeof(LineEditLinkage));
+    }
+  return linkages;
+}
+
 void ledit_init()
 {
-  if (!line)
+  if (!ledit_line)
     {
       ledit_alloc();
     }
   else
     {
-      memset(line,0,sizeof(LineEditRecord));
+      memset(ledit_line,0,sizeof(LineEditRecord));
     }
 
-  num_lines=0;
+  ledit_num_lines=0;
 }
 
 void ledit()
